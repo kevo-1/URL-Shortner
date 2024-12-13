b3131e37944f148cfcb35d3767c53b015d9b8211
kevo-1 <kevin.bastawrous@gmail.com>
2024-12-13 14:48:36 +0200

    added temp main and validation function

--- Utils/validation.go
+++ Utils/validation.go
@@ -1,1 +1,6 @@
 package utils
+
+func validateString(gvnHash string, str string) bool {
+	MD5Hash(str)
+	return gvnHash == MD5Hash(str)
+}
--- main.go
+++ main.go
@@ -1,5 +1,14 @@
-package urlshortner
+package main
+
+import (
+	"fmt"
+	"UrlShortner/utils"
+)
 
 func main() {
-	//TODO
+	fmt.Println("Enter the link you want to shorten: ")
+	var URL string
+	fmt.Scanln(&URL)
+	hashed := utils.MD5Hash(URL)
+	fmt.Println("Original Link: ", URL,"\nShortened Link: ", hashed)
 }
